b69446bdec98b22aba422ccad7ef13ca3aa82289
Murager <jmmukanov@gmail.com>
2023-12-04 17:37:18 +0600

    compose changes

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://shop_admin:488719@db:5432/shop_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://shop_admin:488719@psql:5432/shop_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
